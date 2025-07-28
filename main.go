04527d28bee057d56d1dd4cec2d81468e6c040e1
SnK <kelvintrinh09@gmail.com>
2025-07-28 15:39:13 +0700

    refactor main.go to simplify imports and introduce Vertex struct

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
-
-	for i := 1; i <= 5; i++ {
-		fmt.Println("i =", 100/i)
-	}
+	fmt.Println(Vertex{1, 2})
 }
