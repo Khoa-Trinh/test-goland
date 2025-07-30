f95fc997b03df70004c0a185269c5d3a564f22d9
SnK <kelvintrinh09@gmail.com>
2025-07-30 18:39:32 +0700

    implement Image struct and color model for custom image generation

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
 
-type Vertex struct {
-	Lat, Long float64
+type Image struct {
+	width, height int
 }
 
-var m map[string]Vertex
+func (m Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (m Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, m.width, m.height)
+}
+
+func (m Image) At(x, y int) color.Color {
+	if x < 0 || x >= m.width || y < 0 || y >= m.height {
+		return color.RGBA{} // Return transparent for out of bounds
+	}
+	// Create a color based on the pixel position
+	r := uint8(x % 256)
+	g := uint8(y % 256)
+	b := uint8((x + y) % 256)
+	return color.RGBA{R: r, G: g, B: b, A: 255} // Fully opaque
+}
 
 func main() {
-	m = make(map[string]Vertex)
-	m["Bell Labs"] = Vertex{
-		40.68433, -74.39967,
-	}
-	fmt.Println(m["Bell Labs"])
+	m := Image{1024, 1024}
+	pic.ShowImage(m)
 }
